ce9d62c0bd9cb2d10a28c8d6f39f3dc75a32d0b4
KeisukeSDN <0xu79f313262y6k@ezweb.ne.jp>
2020-03-17 17:09:21 +0900

    mysql接続に必要なパッケージを追加

--- techjam/settings.py
+++ techjam/settings.py
@@ -27,10 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    'django_mysql', #JSON入れるために必要
+    'webapp.apps.WebappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
