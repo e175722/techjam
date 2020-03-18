de92c238e769b3b9aad390d868f32b76cb47d23f
KeisukeSDN <0xu79f313262y6k@ezweb.ne.jp>
2020-03-19 04:58:37 +0900

    detail画面修正

--- webapp/views.py
+++ webapp/views.py
@@ -16,25 +16,6 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Employee
 
-    def get_context_data(self, **kwargs):
-        IntroductionsCategories = IntroductionCategory.objects.all()
-        IntroductionCategoryById = {}
-
-        for Category in IntroductionsCategories:
-            IntroductionCategoryById[str(Category.id)] = Category.category_name
-
-        tags = TagCategory.objects.all()
-        tagById = {}
-        for tag in tags:
-            tagById[str(tag.id)] = tag.category
-
-
-        context = super().get_context_data(**kwargs) # はじめに継承元のメソッドを呼び出す
-        context ["IntroductionCategoryById"] = IntroductionCategoryById
-        context ["tagById"] = tagById
-        return context
-
-
 
 def form_upload(request):
     form = UploadForm(request.POST, request.FILES)
@@ -44,15 +25,3 @@ def form_upload(request):
     else:
         form = UploadForm()
         return render(request,'webapp/add_employee.html', {'form': form})
-
-
-'''
-def form_upload(request):
-    form = UploadForm(request.POST, request.FILES)
-    if request.method == 'POST' and form.is_valid():
-        form.save()
-        return redirect('webapp:index')
-    else:
-        form = UploadForm()
-        return render(request,'webapp/add_employee.html', {'form': form})
-'''
