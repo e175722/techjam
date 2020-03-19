845169006344a006d296f1fb0981d84763701f02
KeisukeSDN <0xu79f313262y6k@ezweb.ne.jp>
2020-03-19 16:05:53 +0900

    Update & Delete機能追加

--- webapp/urls.py
+++ webapp/urls.py
@@ -6,6 +6,8 @@ app_name = 'webapp'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('detail/<int:pk>', views.DetailView.as_view(), name='detail'),
-    path('add/',views.form_upload, name='add'),
-    #path('success/url/',views.success),
+    path('adminTop/add/',views.form_upload, name='add'),
+    path('adminTop/',views.IndexAdminView.as_view(), name='admin'),
+    path('adminTop/update/<int:pk>',views.UpdateView.as_view(), name='update'),
+    path('adminTop/delete/<int:pk>/',views.DeleteView.as_view(), name='delete'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -13,6 +13,24 @@ class IndexView(generic.ListView):
     paginate_by = 6
 
 
+class IndexAdminView(generic.ListView):
+    model = Employee
+    paginate_by = 6
+    template_name = 'webapp/admin_top.html'
+
+
+class DeleteView(generic.DeleteView):
+    model = Employee
+    success_url = reverse_lazy('webapp:index')
+
+
+class UpdateView(generic.UpdateView):
+    model = Employee
+    form_class = UploadForm
+    success_url = reverse_lazy('webapp:index')
+    template_name = 'webapp/update_employee.html'
+
+
 class DetailView(generic.DetailView):
     model = Employee
 
