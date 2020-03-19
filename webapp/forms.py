dec3ea66480303faefdfa246b37c10849b6e1461
KeisukeSDN <0xu79f313262y6k@ezweb.ne.jp>
2020-03-19 16:03:45 +0900

    created_atとupdated_atをformから消した

--- webapp/forms.py
+++ webapp/forms.py
@@ -6,3 +6,9 @@ class UploadForm(forms.ModelForm):
     class Meta:
         model = Employee
         fields = '__all__'
+        exclude = ('created_at','updated_at','mail')
+
+    def __init__(self, *args, **kwargs):
+        super(UploadForm, self).__init__(*args, **kwargs)
+        self.fields['introduction_3'].required = False
+        self.fields['introduction_2'].required = False
